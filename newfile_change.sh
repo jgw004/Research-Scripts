08c409c30ab170aea88ab4be832fdfe4daf0d60e
jgw004 <williamson_j@rocketmail.com>
2024-04-11 12:27:55 -0500

    resolved newfile_change.sh

--- newfile_change.sh
+++ newfile_change.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ### This script calls run_jobs.sh after the changes are made to the files.
-
+$RUN_FILE="run_file"
 TOP_PATH=$(pwd)
 
 # Naming your directories
-a=("dir1" "dir2" "dir3")
+a=("dir1" "dir2" "dir3" "dir4" "dir5" "dir6" "dir7")
 for i in ${a[@]} ; do
 
 # Name the path to run the batch file
@@ -13,26 +13,24 @@ BATCH_PATH="${i}7kms/ch*/namd"
 cd $BATCH_PATH
 echo "------ ${i} -------"
 
-## change file contents of your {run_file} based on line contents
-sed -i "s/\(#SBATCH -J \)wild_type/\1${i}_$k/" "run_file"	    ##job name
-sed -i "s/\(#SBATCH -N \)64/\152/" "run_file"			        ##node
-sed -i "s/\(#SBATCH -n \)64/\152/" "run_file"			        ##node number
-sed -i "s/\(#SBATCH -p \)development/\1normal/" "run_file"      ##job type
-sed -i "s/\(#SBATCH -t \)24/\148/" "run_file"			        ##run-time (hours)
-sed -i "s/\(ibrun namd2 .*\)step5_production26\.inp/\1step5_production27.inp/" "run_file"       ##input file name
-sed -i "s/\(ibrun namd2 .*\)step5.26_production\.log/\1step5.27_production.log/" "run_file"     ##log file name
-
-## replace file conents of step5.1.*_development.inp
-#sed -i "s/\(outputName[[:space:]]\+step5\.1\.\)40_development/\1${k}_development/" "$file_path_input"
+## change file contents of your run_file based on line contents
+sed -i "s/\(#SBATCH -J \)wild_type/\1${i}_$k/" "$RUN_FILE"	    ##job name
+sed -i "s/\(#SBATCH -N \)64/\152/" "$RUN_FILE"			        ##node
+sed -i "s/\(#SBATCH -n \)64/\152/" "$RUN_FILE"			        ##node number
+sed -i "s/\(#SBATCH -p \)development/\1normal/" "$RUN_FILE"      ##job type
+sed -i "s/\(#SBATCH -t \)24/\148/" "$RUN_FILE"			        ##run-time (hours)
+sed -i "s/\(ibrun namd2 .*\)step5_production26\.inp/\1step5_production27.inp/" "$RUN_FILE"       ##input file name
+sed -i "s/\(ibrun namd2 .*\)step5.26_production\.log/\1step5.27_production.log/" "$RUN_FILE"     ##log file name
 
 
 ####################
 ##### CHECKING #####
-grep -Ei 'sbatch -N|SBATCH -t|ibrun namd2' run_file > check-contents.txt
-cp check-contents.txt $TOP_PATH  #to compare after theyre done
+## Stores the run_file contents of the grepped parts from the last iteration
+grep -Ei 'sbatch -N|SBATCH -t|ibrun namd2' $RUN_FILE > check-contents.txt
+cp check-contents.txt $TOP_PATH
 
 #echo "----${i}----"
-#grep -Ei "ibrun namd2" run_file
+#grep -Ei "ibrun namd2" $RUN_FILE
 #echo ""
 #ls $TOP_PATH/$BATCH_PATH/step5.26_production.log
 
@@ -47,7 +45,7 @@ done
 echo ""
 cat check-contents.txt
 echo ""
-echo "Changes to 'run_file' were applied successfully!"
+echo "Changes to '$RUN_FILE' were applied successfully!"
 echo ""
 #####################
 ./run_jobs.sh
